1820e81d6df8f7c16409f91e5ef923100db97955
AntonijoLuksic <antonijo.luksic@gmail.com>
2022-08-01 21:42:08 +0200

    Rijesen Zadatak04

---
+++ Zadatak04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert year: ");
+            int year=int.Parse(Console.ReadLine());
+
+            if ((year%4==0&&year%100!=0)||year%400==0)
+            {
+                Console.WriteLine("Year is leap");
+            }
+            else
+            {
+                Console.WriteLine("Year is common");
+            }
+        }
+    }
+}
