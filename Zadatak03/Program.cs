7606ac534af566c7ee867691e75dd2f7119118e2
AntonijoLuksic <antonijo.luksic@gmail.com>
2022-08-01 21:25:19 +0200

    Rijesen Zadatak03

--- Zadatak03/Program.cs
+++ Zadatak03/Program.cs
@@ -15,7 +15,41 @@ namespace Zadatak03
             do
             {
                 Console.Clear();
-            } while (true);
+
+                Console.Write("Insert first number: ");
+                double a=double.Parse(Console.ReadLine());
+
+                Console.Write("Insert second number: ");
+                double b=double.Parse(Console.ReadLine());
+
+                Console.Write("Insert operation (+,-,*,/): ");
+
+                string op=Console.ReadLine();
+
+                switch (op)
+                {
+                    case "+":
+                        Console.WriteLine($"{a} {op} {b} = {a+b}");
+                        break;
+                        case "-":
+                        Console.WriteLine($"{a} {op} {b} = {a - b}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{a} {op} {b} = {a - b}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"{a} {op} {b} = {a - b}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong operation!");
+                        break;
+                }
+
+                Console.WriteLine("Repeat (y/n)?");
+
+                repeat=Console.ReadKey().KeyChar;
+            } while (repeat=='y');
         }
     }
 }
