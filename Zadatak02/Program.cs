cce50251c88ee076b7821b9238d3e5f22eecdc22
AntonijoLuksic <antonijo.luksic@gmail.com>
2022-08-01 21:17:30 +0200

    Add project files.

---
+++ Zadatak02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert first number: ");
+            int a=int.Parse(Console.ReadLine());
+
+            Console.Write("Insert second number: ");
+            int b=int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a} * {b}= {a*b}");
+            Console.WriteLine($"{a} / {b}= {a/b}");
+
+            Console.WriteLine($"{{test}}");
+
+            Console.WriteLine("\"test\"");
+
+            Console.WriteLine("\\test\\");
+
+            Console.ReadKey();
+
+        }
+    }
+}
