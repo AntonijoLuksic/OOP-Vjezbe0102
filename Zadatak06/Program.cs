4095d8dc93eded1d2a338514b99cbcd0c0d0ba72
AntonijoLuksic <antonijo.luksic@gmail.com>
2022-08-02 19:06:40 +0200

    Zadatak 06 rijesen rekurzijom

---
+++ Zadatak06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert positive number: ");
+            int n=int.Parse(Console.ReadLine());
+            long factorial = Factorial(n);
+            Console.WriteLine($"{n}!= {factorial}");
+        }
+
+        private static long Factorial(int n)
+        {
+            if (n<=1)
+            {
+                return 1; //bazni slucaj koji prekida rekurzivno pozivanje metode
+            }
+            return n * Factorial(--n); //rekurzija
+        }
+    }
+}
